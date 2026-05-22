c9c242bf584782839da244dc664aa85268414b1a
kickbell <dev.kickbell@gmail.com>
2026-05-22 11:22:24 +0900

    feat: db 설정, 서버 정상 실행 테스트

--- src/main/java/com/jongchan/rule_engine_playground/RuleEnginePlaygroundApplication.java
+++ src/main/java/com/jongchan/rule_engine_playground/RuleEnginePlaygroundApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RuleEnginePlaygroundApplication {
 
 	public static void main(String[] args) {
+		System.out.println("서버가 잘 실행되는 중!!");
 		SpringApplication.run(RuleEnginePlaygroundApplication.class, args);
 	}
 
