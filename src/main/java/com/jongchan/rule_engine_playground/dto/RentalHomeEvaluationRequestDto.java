fc6ba676e3e044782a3fcda0eeaf30506699799a
kickbell <dev.kickbell@gmail.com>
2026-05-22 17:40:33 +0900

    feat: 룰 신청알림 DTO 추가

---
+++ src/main/java/com/jongchan/rule_engine_playground/dto/RentalHomeEvaluationRequestDto.java
@@ -0,0 +1,51 @@
+package com.jongchan.rule_engine_playground.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RentalHomeEvaluationRequestDto {
+
+    // 11가지 신청자 청약 자격 정보 (Flat 구조로 프론트엔드가 매개변수 형태로 입력 가능)
+    private Boolean isHomeOwner;
+    private Boolean hasPastHomeOwnership;
+    private Integer age;
+    private String residence;
+    private Double marriageDurationYears;
+    private Integer incomePercent;
+    private Long totalAsset;
+    private Long carValue;
+    private Integer subscriptionCount;
+    private List<String> specialQualifications;
+    private String pastContractHistory;
+
+    /**
+     * DTO의 평탄한(Flat) 신청자 데이터 구조를
+     * 규칙 엔진이 요구하는 중첩 구조(user.xxx)의 Facts Map 포맷으로 변환합니다.
+     */
+    public Map<String, Object> toFactsMap() {
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("isHomeOwner", this.isHomeOwner);
+        userMap.put("hasPastHomeOwnership", this.hasPastHomeOwnership);
+        userMap.put("age", this.age);
+        userMap.put("residence", this.residence);
+        userMap.put("marriageDurationYears", this.marriageDurationYears);
+        userMap.put("incomePercent", this.incomePercent);
+        userMap.put("totalAsset", this.totalAsset);
+        userMap.put("carValue", this.carValue);
+        userMap.put("subscriptionCount", this.subscriptionCount);
+        userMap.put("specialQualifications", this.specialQualifications);
+        userMap.put("pastContractHistory", this.pastContractHistory);
+
+        Map<String, Object> factsMap = new HashMap<>();
+        factsMap.put("user", userMap);
+        return factsMap;
+    }
+}
