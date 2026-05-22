451be9415cd67ad3caf8eb7f799709289bb26846
kickbell <dev.kickbell@gmail.com>
2026-05-22 10:54:36 +0900

    init

---
+++ src/main/java/com/jongchan/rule_engine_playground/RuleEnginePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jongchan.rule_engine_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEnginePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleEnginePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongchan/rule_engine_playground/RuleEnginePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongchan.rule_engine_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEnginePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
